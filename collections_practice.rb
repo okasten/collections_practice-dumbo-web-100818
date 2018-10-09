360fc901ba6f4436c80f0be2036e3cbd6d2c1638
Olivia Kasten <odkasten@gmail.com>
2018-10-09 01:27:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,10 @@ end
 def sort_array_char_count(arr)
   arr.sort {|x, y| x.length <=> y.length}
 end 
+
+def swap_elements(arr)
+  second_element = arr[1]
+  third_element = arr[2]
+  arr[1] = third_element
+  arr[2] = second_element
+end 
